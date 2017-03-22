af2a12e54348fbbb4ae326b05e6497b58fc704e6
Luis Baquera <luis.baquera@gmail.com>
2017-03-21 17:40:47 -0700

    comments

--- find_landscape_imgs.py
+++ find_landscape_imgs.py
@@ -18,17 +18,23 @@ for File in sys.argv:
 	tree = ET.parse(File)
 	
 	with open(File,'r') as f:
+		# read data form file into "data"
 		data = f.read()
+
+		# parse the file in memory
 		root = ET.parse(StringIO(data))
 		
+		# get the Page element and store the HEIGHT and WIDTH attributes in respective vars
 		for page in root.getiterator('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
 			height = page.attrib['HEIGHT']
 			width  = page.attrib['WIDTH']
-				
+			
+			# compare height and width; if width > height, landscape detected. print a message to that effect
 			if height > width:
 				continue  
 			else: 
 				landscape_file_count = landscape_file_count + 1
 				print File + ": " + height + " x " + width + "; landscape detected."
+
 print "Analysis complete. " + str(landscape_file_count) + (" file " if landscape_file_count == 1 else " files ") + "found in landscape orientation"
 
